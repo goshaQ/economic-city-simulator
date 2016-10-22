0319a5fcaf0d6af9f2632034f1ced03785bcd9d2
gosha <gosha97-19@mail.ru>
2016-10-22 21:05:54 +0300

    initiated creation structure of game

---
+++ core/src/com/itproject/game/City.java
@@ -0,0 +1,13 @@
+package com.itproject.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class City extends ApplicationAdapter {
+    public City() {
+
+    }
+
+    public void checkEvents() {
+
+    }
+}
